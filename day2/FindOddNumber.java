506bdc527fe3a48c7f5ba959115f8b197841152a
Lakshmi Priya <lakshmipriya941993@gmail.com>
2024-06-18 22:19:59 +0530

    Add files via upload
    Day2

---
+++ day2/FindOddNumber.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+import java.util.Iterator;
+
+public class FindOddNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num =10;
+		for (int i = 0; i <=10; i++) {
+			if(i%2!=0) {
+				System.out.println(i);
+			}
+			
+		}
+	}
+
+}
