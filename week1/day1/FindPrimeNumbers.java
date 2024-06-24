838be163eb5b89e91cc15edaf55413e3dc1bd020
Lakshmi Priya <lakshmipriya941993@gmail.com>
2024-06-24 07:36:30 +0530

    Add files via upload

---
+++ week1/day1/FindPrimeNumbers.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class FindPrimeNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 10;
+				
+		if(num%2==1) {
+			System.out.println("This is prime number"+num);
+			
+			
+		}
+		System.out.println("This is not a prime number");
+	}
+
+}
