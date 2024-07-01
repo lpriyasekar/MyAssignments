099a43f3378ec346cb9052212aa772078de8f7f8
Lakshmi Priya <lakshmipriya941993@gmail.com>
2024-07-01 07:00:42 +0530

    Add files via upload

---
+++ week2/day3/Browser.java
@@ -0,0 +1,21 @@
+package week2.day3;
+
+
+public class Browser {
+
+	void openURL()
+	{
+		System.out.println("Browser name:");
+	}
+	
+	void closeBrwoser()
+	{
+		System.out.println("Brwoser Version");
+	}
+
+	void navigateBrowser()
+	{
+		System.out.println("Navigate Back");
+	}
+}
+
---
+++ week2/day4/DatabseConnection.java
@@ -0,0 +1,14 @@
+package week2.day4;
+
+public interface DatabseConnection {
+
+	
+	public void connect();
+	
+	public void disconnect();
+	
+	public void executeUpdate();
+	
+	
+
+}
